851f09bff80137d550e478f5f6f71973c79fc83d
f-rahman23 <f.rahman@iideainformatics.com>
2023-07-31 10:21:45 +0600

    Now, Login functionality is working!

--- frontend/src/axiosClient.ts
+++ frontend/src/axiosClient.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
+
 const client = axios.create({
-	baseURL: 'http://localhost:3001',
+	baseURL: 'http://127.0.0.1:3001',
 	withCredentials: true,
 	timeout: 3000,
-	xsrfCookieName: 'csrftoken',
-	xsrfHeaderName: 'X-CSRFToken',
-
+	xsrfCookieName: "csrftoken",
+	xsrfHeaderName: 'X-CSRFToken'
 });
 
 export default client;
